0191dc2db547a677e5e9af33f39642e4637be9cf
daniel <daniel_demon@live.ru>
2014-08-18 11:48:06 +0300

    source update

---
+++ source/core/MY_Controller.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+class MY_Controller extends CI_Controller {
+    public  $user = false;
+    function __construct() {
+        parent::__construct();
+        if ($this->uri->segment(1) !== 'admin') {
+            $this->layout = new Layout(array('siteSide' => 'frontend','themeName' => 'main'));
+        }
+    }
+}
